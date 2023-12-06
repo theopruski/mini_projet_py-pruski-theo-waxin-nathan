2243314aec31262d529871b526d1a8d1cea67a6f
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2023-12-06 10:06:20 +0100

    supprimier tableau des longitude

--- main.py
+++ main.py
@@ -31,24 +31,6 @@ def recup_contry(url, name_csv):
 
 
 info_pays = 'https://developers.google.com/public-data/docs/canonical/countries_csv'
-recup_contry(info_pays, "./data/country_coord.csv")
-df = pd.read_csv("./data/country_coord.csv")
-selected_columns = ['name', 'latitude', 'longitude']
-df_selected = df[selected_columns]
-fig = px.choropleth(df_selected,
-                    locations='name',  # Utilisez le nom du pays comme identifiant de lieu
-                    # Colorez en fonction de la latitude (ajustez en fonction de vos besoins)
-                    color='latitude',
-                    hover_name='name',  # Affichez le nom du pays au survol
-                    title='Carte du Monde avec Frontières et Scores de Liberté de la Presse',
-                    projection='natural earth',  # Projection de la carte
-                    color_continuous_scale=[
-                        'green', 'yellow', 'orange', 'red', 'purple'],
-                    )
-fig.update_geos(showcountries=True, countrycolor="Black")
-fig.add_trace(px.scatter_geo(df_selected, lat='latitude',
-              lon='longitude').data[0])
-graph_component = dcc.Graph(id='world-map', figure=fig)
 
 # fig = px.scatter_geo(df_selected, lat='latitude', lon='longitude',
 # text='name', projection='natural earth')
@@ -76,23 +58,6 @@ fig = px.choropleth(df_selected_cla,
 if __name__ == '__main__':
     app = dash.Dash(__name__)
     app.layout = html.Div([
-        html.H1("Tableau des libertés"),
-        dash_table.DataTable(
-            id='table',
-            columns=[{"name": col, "id": col} for col in df_selected.columns],
-            data=df_selected.to_dict('records'),
-            style_cell_conditional=[
-                {'if': {'column_id': 'name'}, 'width': '10%'},
-            ],
-            style_table={'width': '30%'},
-            style_cell={'textAlign': 'left'},
-        ),
-        html.H1("Carte du monde avec les coordonnées GPS des pays"),
-        dcc.Graph(
-            id='world-map',
-            figure=fig,
-        ),
-
         html.H1("Classement 2023 de la liberté de la presse"),
         dash_table.DataTable(
             id='table-classement',
@@ -102,6 +67,13 @@ if __name__ == '__main__':
             style_table={'width': '30%'},
             style_cell={'textAlign': 'left'},
         ),
+        html.H1("Carte du monde avec les coordonnées GPS des pays"),
+        dcc.Graph(
+            id='world-map',
+            figure=fig,
+        ),
+
+        
     ])
 
     app.run_server(debug=True)
