ce8e2ab4f7d1ce0c3248dea808e52d473aa82281
theopruski <theo.pruski@gmail.com>
2023-12-13 12:47:34 +0100

    feat main.py - fusion coordonnées / score press

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dash import html
 from dash import dash_table
 import io
 
-# from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output
 import csv
 import pandas as pd
 import requests
@@ -31,24 +31,34 @@ def recup_contry(url, name_csv):
 
 
 info_pays = 'https://developers.google.com/public-data/docs/canonical/countries_csv'
+recup_contry(info_pays, "./data/country_coord.csv")
+df = pd.read_csv("./data/country_coord.csv")
+selected_columns = ['name', 'latitude', 'longitude']
+df_selected = df[selected_columns]
 
 # fig = px.scatter_geo(df_selected, lat='latitude', lon='longitude',
 # text='name', projection='natural earth')
 
 # url_classement = 'https://rsf.org/sites/default/files/import_classement/2023.csv'
 df_classement = pd.read_csv(
-    "./data/country_press_global_score_2023_fr.csv", delimiter=';', decimal=',')
-selected_columns_classement = ['ISO','Country_FR', 'Score', 'Rank']
+    "./data/country_press_global_score_2023.csv", delimiter=';', decimal=',')
+selected_columns_classement = [
+    'ISO', 'Country_FR', 'Country_EN', 'Score', 'Rank']
 df_selected_cla = df_classement[selected_columns_classement]
-fig = px.choropleth(df_selected_cla,
-                    locations='ISO', 
+
+merged_df = pd.merge(df_selected, df_selected_cla,
+                     how='inner', left_on='name', right_on='Country_EN')
+
+fig = px.choropleth(merged_df,
+                    locations='ISO',
                     color='Score',
                     hover_name='Country_FR',
-                    hover_data={'ISO': False, 'Country_FR': False, 'Score': True, 'Rank': True},
+                    hover_data={'ISO': False, 'Country_FR': False, 'Country_EN': False,
+                                'Score': True, 'Rank': True, 'latitude': True, 'longitude': True},
                     title='Carte du Monde avec Frontières et Scores de Liberté de la Presse',
                     projection='natural earth',
                     color_continuous_scale=[
-                        'black', 'red', 'orange', 'yellow', 'green'], 
+                        'black', 'red', 'orange', 'yellow', 'green'],
                     scope='world',
                     width=1200,
                     height=800,
@@ -57,8 +67,25 @@ fig = px.choropleth(df_selected_cla,
 # info_press = 'https://rsf.org/sites/default/files/import_classement/2023.csv'
 # recup_liberte_press(info_press, ".data/country_press.csv")
 
+app = dash.Dash(__name__)
+
+
+@app.callback(
+    Output('country-info', 'children'),
+    Input('world-map', 'clickData'))
+def update_country_info(clickData):
+    if clickData is None:
+        return "Cliquez sur un pays pour voir plus d'informations."
+    else:
+        country_iso = clickData['points'][0]['location']
+        country_row = df_selected_cla[df_selected_cla['ISO'] == country_iso]
+        country_name = country_row['Country_FR'].values[0]
+        country_score = country_row['Score'].values[0]
+        country_rank = country_row['Rank'].values[0]
+        return f"Vous avez sélectionné {country_name}. Son score de liberté de la presse est {country_score} et son rang est {country_rank}."
+
+
 if __name__ == '__main__':
-    app = dash.Dash(__name__)
     app.css.append_css({
         'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
     })
@@ -67,22 +94,24 @@ if __name__ == '__main__':
         dash_table.DataTable(
             id='table-classement',
             columns=[
-                {"name": "Nom", "id": "Country_FR"}, 
-                {"name": "Score", "id": "Score"}, 
-                 {"name": "Rang", "id": "Rank"}, 
+                {"name": "Nom", "id": "Country_EN"},
+                {"name": "Score", "id": "Score"},
+                {"name": "Rang", "id": "Rank"},
             ],
-            data=df_selected_cla[['Country_FR', 'Score', 'Rank']].to_dict('records'), 
+            data=df_selected_cla[['Country_EN',
+                                  'Score', 'Rank']].to_dict('records'),
             sort_action='native',
             page_action='native',
-            page_current=0, 
+            page_current=0,
             page_size=15,
             style_cell={'textAlign': 'left'},
-        ), 
+        ),
         html.H1("Carte du monde avec les coordonnées GPS des pays"),
         dcc.Graph(
             id='world-map',
             figure=fig,
         ),
+        html.Div(id='country-info')
     ], style={'marginRight': 200})
 
     app.run_server(debug=True)
